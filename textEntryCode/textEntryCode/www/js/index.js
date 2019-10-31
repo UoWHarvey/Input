ba019a6e91e4b1734d53b16ca960c5f3b6284ce7
UoWHarvey <55839084+UoWHarvey@users.noreply.github.com>
2019-10-31 14:00:17 +0000

    storage
    storage

--- textEntryCode/textEntryCode/www/js/index.js
+++ textEntryCode/textEntryCode/www/js/index.js
@@ -8,8 +8,12 @@ $(document).on("pagecreate","#pageone",function(){
 function submitText() {
 	var text = $('#textinput').val();
 	alert(text);
+    storeValue('alert', text);
 }
 
 function storeValue(key, value) {
+    localStorage.setItem (key, value);
+
+
 	//add some code to store the key-value pair in persistant storage 
 }
